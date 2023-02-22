f61e8f082ca62a8bc8f0e685d9b71a9cdf454307
Dmytro Myrhorodskyi <enigmatic992@gmail.com>
2023-02-22 13:53:57 +0200

    Registration functionality

--- accounts/urls.py
+++ accounts/urls.py
@@ -6,5 +6,4 @@ urlpatterns = [
     path('registration/', views.registration_view, name='registration'),
     path('login/', views.login_view, name='login'),
     path('logout/', views.logout_view, name='logout'),
-    # path('activate/<uidb64>/<token>/', views.activate, name='activate'),
 ]
