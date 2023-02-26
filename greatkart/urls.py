ac485fa949fdc634c823dc2e765b41e46202e2f0
Dmytro Myrhorodskyi <enigmatic992@gmail.com>
2023-02-26 16:06:42 +0200

    Product order and payments

--- greatkart/urls.py
+++ greatkart/urls.py
@@ -24,7 +24,8 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('store/', include('store.urls')),
     path('cart/', include('carts.urls')),
-    path('accounts/', include('accounts.urls'))
+    path('accounts/', include('accounts.urls')),
+    path('orders/', include('orders.urls')),
 ]
 
 if settings.DEBUG:
